2cef1668edb56b386e9f2db5314b55bb9d69414e
andreysulymka <andreysulymka5@gmail.com>
2023-03-14 00:42:57 +0200

    Update FeedbackButtons.styled.js

--- src/components/FeedbackButtons/FeedbackButtons.styled.js
+++ src/components/FeedbackButtons/FeedbackButtons.styled.js
@@ -8,9 +8,10 @@ export const Button = styled.button`
 padding: 10px;
 cursor: pointer;
 font-weight: bold;
-background-color: aqua;
+background-color: rgba(68, 115, 150, 0.6);;
 border: none;
  border-radius: 25px;
  :hover{
-    background-color: rgba(234, 223, 139, 0.8);}
+    background-color: rgba(216, 222, 146, 0.6);}
 `
+
